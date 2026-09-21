d46290e94f0e788b56928f29a4898f9d24327ed2
“[pogula <[pogulaprashanthyadav143@gmail.com]>
2026-09-21 13:06:31 +0530

    Authorize job applications with user session

--- kyk-technologies/static/js/dash-common.js
+++ kyk-technologies/static/js/dash-common.js
@@ -54,10 +54,10 @@ on('loginBtn','click',async()=>{
     if(!res.ok) throw new Error(d.error || 'Login failed');
     if (isAdminLogin || d.role && ROLE_HOME[d.role]) {
       gToken=d.token; localStorage.setItem(TOKEN_KEY,gToken);
-      location.replace('/index.html');
+      location.replace(loginBtn.dataset.redirect || '/index.html');
     } else {
       localStorage.setItem(LOGIN_USER_TOKEN_KEY, d.token);
-      location.replace('/index.html');
+      location.replace(loginBtn.dataset.redirect || '/index.html');
     }
   } catch(e){ setFormMsg(msg,e.message,false); }
 });
